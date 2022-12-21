ec6ebb0499dee6d214e43435119396a5248d84a1
Amit Aggarwal <amitagg3@gmail.com>
2022-12-22 00:54:49 +0530

    first commit

---
+++ src/main/java/com/amit/DockerCheck/DockerCheckApplication.java
@@ -0,0 +1,21 @@
+package com.amit.DockerCheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class DockerCheckApplication {
+	@GetMapping("/message")
+	public String getProjectName(){
+		return "Hello Amit! This is docker project check.";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amit/DockerCheck/DockerCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amit.DockerCheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
